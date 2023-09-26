cec37fc0c4db3db4e4ec3908fe51813dbc04f717
Edijs <edijsapse@gmail.com>
2023-09-26 12:43:07 +0300

    Store records in table container and blob container

--- Atea/Function1.cs
+++ Atea/Function1.cs
@@ -1,19 +1,79 @@
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Atea
 {
     public class Function1
     {
-        const string ENDPOINT = "https://api.publicapis.org/random?auth=null";
-
         [FunctionName("Function1")]
-        public void Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var http = new HttpClient();
+
+            var data = await http.GetAsync(Environment.GetEnvironmentVariable("APIEndpoint"));
+
+            var success = data.IsSuccessStatusCode;
+            var payload = await data.Content.ReadAsStringAsync();
+
+            var blobId = CreateBlobRecord(payload);
+
+            CreateTableRecord(success, blobId.Result);
+
+            log.LogInformation($"Records Created!");
+        }
+
+        public async Task<string> CreateBlobRecord(string responsePayload)
+        {
+            var blobServiceClient = new BlobServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+            var nameOfFile = Guid.NewGuid().ToString();
+
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(Environment.GetEnvironmentVariable("StorageName"));
+
+            if (!containerClient.Exists())
+            {
+                containerClient = await blobServiceClient.CreateBlobContainerAsync(Environment.GetEnvironmentVariable("StorageName"));
+            }
+
+            string localPath = "data";
+            Directory.CreateDirectory(localPath);
+
+            string fullFileName = nameOfFile;
+            string localFilePath = Path.Combine(localPath, fullFileName);
+
+            await File.WriteAllTextAsync(localFilePath, responsePayload);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fullFileName);
+
+            await blobClient.UploadAsync(localFilePath, true);
+
+            return nameOfFile;
+        }
+
+        public async void CreateTableRecord(bool requestWasSuccessfull, string blobContainersName)
+        {
+            var tableServiceClient = new TableServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+
+            var tableClient = tableServiceClient.GetTableClient(Environment.GetEnvironmentVariable("StorageName"));
+
+            await tableClient.CreateIfNotExistsAsync();
+
+            var id = Guid.NewGuid().ToString();
+
+            var record = new Record
+            {
+                RowKey = id,
+                PartitionKey = id,
+                WasSuccessfull = requestWasSuccessfull,
+                BlobContainersID = blobContainersName
+            };
+
+            await tableClient.AddEntityAsync(record);
         }
     }
 }
---
+++ Atea/Record.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace Atea
+{
+    public class Record : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+
+        public string RowKey { get; set; }
+
+        public DateTimeOffset? Timestamp { get; set; }
+
+        public ETag ETag { get; set; }
+
+        public bool WasSuccessfull { get; set;}
+
+        public string BlobContainersID { get; set; }
+    }
+}
